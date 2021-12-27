0914b904c307b29afe0863cc0e511c8d55a5a043
Ashish Gautam <52909024+engineeringstudent0012@users.noreply.github.com>
2021-12-27 20:37:46 +0545

    Create symbols.py

---
+++ text/symbols.py
@@ -0,0 +1,11 @@
+from text import cmudict
+
+_pad        = '_'
+_punctuation = '!\'(),.:;? '
+_special = '-'
+_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+
+_arpabet = ['@' + s for s in cmudict.valid_symbols]
+
+# Export all symbols:
+symbols = [_pad] + list(_special) + list(_punctuation) + list(_letters) + _arpabet
